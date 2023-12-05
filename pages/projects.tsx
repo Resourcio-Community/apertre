0419357c8e71722c0472148ab915b48795ef310b
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2023-12-05 20:01:46 +0530

    Added the Projects Section (#3) (#6)
    * feat: packages change
    
    * "Updated Community Partners"
    
    * all partners not qualified yet
    
    * Update Partners.tsx
    
    * updated the community partrner links
    
    ---------
    
    Co-authored-by: Abhi-codes <abhirupsaha02@gmail.com>

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,25 +1,76 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import ProjectCard from 'views/ProjectPage/ProjectCard';
+import Filters from 'views/ProjectPage/Filters'; 
 import { media } from 'utils/media';
 
-const projects = () => {
-  return (
-    <Div>
-      Coming Soon
-    </Div>
-  )
-}
+const projectsData = [
+ 
+  { id: 1, title: 'Project 1', description: 'Description for Project 1', repoUrl:'https://github.com/project1', stars: 100, tags: ['React', 'TypeScript', 'CSS'] ,maintainer: 'Abc'},
+  { id: 2, title: 'Project 2', description: 'Description for Project 2', repoUrl:'https://github.com/project2', stars: 150, tags: ['React', 'JavaScript', 'HTML'], maintainer: 'Abc' },
+  { id: 3, title: 'Project 3', description: 'Description for Project 3', repoUrl:'https://github.com/project3', stars: 200, tags: ['JavaScript', 'CSS'] ,maintainer: 'Abc'},
+  { id: 4, title: 'Project 4', description: 'Description for Project 4', repoUrl:'https://github.com/project4', stars: 200, tags: ['JavaScript', 'CSS'] ,maintainer: 'Abc'},
+  { id: 5, title: 'Project 5', description: 'Description for Project 5', repoUrl:'https://github.com/project5', stars: 100, tags: ['React', 'TypeScript', 'CSS'] ,maintainer: 'Abc'},
+  { id: 6, title: 'Project 6', description: 'Description for Project 6', repoUrl:'https://github.com/project6', stars: 150, tags: ['React', 'JavaScript', 'HTML'],maintainer:'Abc' },
+  { id: 7, title: 'Project 7', description: 'Description for Project 7', repoUrl:'https://github.com/project7', stars: 200, tags: ['JavaScript', 'CSS'] ,maintainer: 'Abc'},
+  { id: 8, title: 'Project 8', description: 'Description for Project 8', repoUrl:'https://github.com/project8', stars: 200, tags: ['JavaScript', 'CSS'] ,maintainer: 'Abc'},
+  { id: 9, title: 'Project 9', description: 'Description for Project 9', repoUrl:'https://github.com/project9', stars: 100, tags: ['React', 'TypeScript', 'CSS'] ,maintainer: 'Abc'},
+  { id: 10, title: 'Project 10', description: 'Description for Project 10', repoUrl:'https://github.com/project10', stars: 150, tags: ['React', 'JavaScript', 'HTML'], maintainer:'Abc'},
+  { id: 11, title: 'Project 11', description: 'Description for Project 11', repoUrl:'https://github.com/project11', stars: 200, tags: ['JavaScript', 'CSS'] ,maintainer: 'Abc'},
+  { id: 12, title: 'Project 12', description: 'Description for Project 12', repoUrl:'https://github.com/project12', stars: 200, tags: ['JavaScript', 'CSS'] ,maintainer: 'Abc'},
+  
+];
 
-const Div = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100vw;
-  height: 30vh;
-  font-size: 4rem;
+const ProjectsPage: React.FC = () => {
+  const [filteredProjects, setFilteredProjects] = useState(projectsData);
+
+  const handleFilterChange = (selectedFilters: string[]) => {
+    if (selectedFilters.length === 0) {
+      setFilteredProjects(projectsData);
+    } else {
+      const filtered = projectsData.filter((project) =>
+        project.tags.some((tag) => selectedFilters.includes(tag))
+      );
+      setFilteredProjects(filtered);
+    }
+  };
+
+  return (
+    <ProjectsContainer>
+      <ProjectsHeader>Our Projects</ProjectsHeader>
+      <Filters onFilterChange={handleFilterChange} />
+      <ProjectsList>
+        {filteredProjects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </ProjectsList>
+    </ProjectsContainer>
+  );
+};
+
+const ProjectsContainer = styled.div`
+  padding: 25rem;
 
   ${media('<=tablet')} {
-    font-size: 3rem;
+    padding: 1rem;
   }
+
+  ${media('<=phone')} {
+    padding: 0.5rem;
+  }
+  
 `;
 
-export default projects
+const ProjectsHeader = styled.h2`
+  font-size: 3.5rem;
+  margin-bottom: 2rem;
+  text-align: center;
+`;
+
+const ProjectsList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 50px; 
+`;
+
+export default ProjectsPage;
