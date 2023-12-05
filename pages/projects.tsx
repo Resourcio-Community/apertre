615330f8b6fd8ea48e609da9b8113fa5b16ccf9d
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2023-12-05 19:23:47 +0530

    Revert "Added the Projects Section (#3)"
    This reverts commit 6ea2064d7fb4c84988a13fd6c6c3fa42a56a3fb3.

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,76 +1,25 @@
-import React, { useState } from 'react';
 import styled from 'styled-components';
-import ProjectCard from 'views/ProjectPage/ProjectCard';
-import Filters from 'views/ProjectPage/Filters'; 
 import { media } from 'utils/media';
 
-const projectsData = [
- 
-  { id: 1, title: 'Project 1', description: 'Description for Project 1', repoUrl:'https://github.com/project1', stars: 100, tags: ['React', 'TypeScript', 'CSS'] ,maintainer: 'Abc'},
-  { id: 2, title: 'Project 2', description: 'Description for Project 2', repoUrl:'https://github.com/project2', stars: 150, tags: ['React', 'JavaScript', 'HTML'], maintainer: 'Abc' },
-  { id: 3, title: 'Project 3', description: 'Description for Project 3', repoUrl:'https://github.com/project3', stars: 200, tags: ['JavaScript', 'CSS'] ,maintainer: 'Abc'},
-  { id: 4, title: 'Project 4', description: 'Description for Project 4', repoUrl:'https://github.com/project4', stars: 200, tags: ['JavaScript', 'CSS'] ,maintainer: 'Abc'},
-  { id: 5, title: 'Project 5', description: 'Description for Project 5', repoUrl:'https://github.com/project5', stars: 100, tags: ['React', 'TypeScript', 'CSS'] ,maintainer: 'Abc'},
-  { id: 6, title: 'Project 6', description: 'Description for Project 6', repoUrl:'https://github.com/project6', stars: 150, tags: ['React', 'JavaScript', 'HTML'],maintainer:'Abc' },
-  { id: 7, title: 'Project 7', description: 'Description for Project 7', repoUrl:'https://github.com/project7', stars: 200, tags: ['JavaScript', 'CSS'] ,maintainer: 'Abc'},
-  { id: 8, title: 'Project 8', description: 'Description for Project 8', repoUrl:'https://github.com/project8', stars: 200, tags: ['JavaScript', 'CSS'] ,maintainer: 'Abc'},
-  { id: 9, title: 'Project 9', description: 'Description for Project 9', repoUrl:'https://github.com/project9', stars: 100, tags: ['React', 'TypeScript', 'CSS'] ,maintainer: 'Abc'},
-  { id: 10, title: 'Project 10', description: 'Description for Project 10', repoUrl:'https://github.com/project10', stars: 150, tags: ['React', 'JavaScript', 'HTML'], maintainer:'Abc'},
-  { id: 11, title: 'Project 11', description: 'Description for Project 11', repoUrl:'https://github.com/project11', stars: 200, tags: ['JavaScript', 'CSS'] ,maintainer: 'Abc'},
-  { id: 12, title: 'Project 12', description: 'Description for Project 12', repoUrl:'https://github.com/project12', stars: 200, tags: ['JavaScript', 'CSS'] ,maintainer: 'Abc'},
-  
-];
-
-const ProjectsPage: React.FC = () => {
-  const [filteredProjects, setFilteredProjects] = useState(projectsData);
-
-  const handleFilterChange = (selectedFilters: string[]) => {
-    if (selectedFilters.length === 0) {
-      setFilteredProjects(projectsData);
-    } else {
-      const filtered = projectsData.filter((project) =>
-        project.tags.some((tag) => selectedFilters.includes(tag))
-      );
-      setFilteredProjects(filtered);
-    }
-  };
-
+const projects = () => {
   return (
-    <ProjectsContainer>
-      <ProjectsHeader>Our Projects</ProjectsHeader>
-      <Filters onFilterChange={handleFilterChange} />
-      <ProjectsList>
-        {filteredProjects.map((project) => (
-          <ProjectCard key={project.id} project={project} />
-        ))}
-      </ProjectsList>
-    </ProjectsContainer>
-  );
-};
+    <Div>
+      Coming Soon
+    </Div>
+  )
+}
 
-const ProjectsContainer = styled.div`
-  padding: 25rem;
+const Div = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 30vh;
+  font-size: 4rem;
 
   ${media('<=tablet')} {
-    padding: 1rem;
+    font-size: 3rem;
   }
-
-  ${media('<=phone')} {
-    padding: 0.5rem;
-  }
-  
 `;
 
-const ProjectsHeader = styled.h2`
-  font-size: 3.5rem;
-  margin-bottom: 2rem;
-  text-align: center;
-`;
-
-const ProjectsList = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  gap: 50px; 
-`;
-
-export default ProjectsPage;
+export default projects
--- views/HomePage/Partners.tsx
+++ views/HomePage/Partners.tsx
@@ -10,9 +10,16 @@ import 'swiper/css/autoplay';
 import 'swiper/css/navigation';
 
 const PARTNER_LOGOS = [
-  { name: 'SC CSE.png', link: 'https://sccsebio.vercel.app/?utm_source=sc+cse' },
-  { name: 'Samarth.png',link: 'https://www.samarthtmsl.live/' },
-  { name: 'GDSC NIET.png', link: 'https://gdsc.community.dev/noida-institute-of-engineering-and-technology-greater-noida/' },
+  'comingsoon.png',
+  'comingsoon.png',
+  'comingsoon.png',
+  'comingsoon.png',
+  'comingsoon.png',
+  'comingsoon.png',
+  'comingsoon.png',
+  'comingsoon.png',
+  'comingsoon.png',
+  'comingsoon.png',
 ];
 
 export default function Partners() {
@@ -35,15 +42,9 @@ export default function Partners() {
         modules={[Autoplay]}
         className="swiper-wrapper"
       >
-        {PARTNER_LOGOS.map((partner) => (
-          <SwiperSlide key={partner.name}>
-            {partner.link ? (
-              <PartnerLink href={partner.link} target="_blank" rel="noopener noreferrer">
-                <NextImage src={`/partners/${partner.name}`} alt={normalizePartnerLogoName(partner.name)} width={80} height={60} />
-              </PartnerLink>
-            ) : (
-              <NextImage src={`/partners/${partner.name}`} alt={normalizePartnerLogoName(partner.name)} width={80} height={60} />
-            )}
+        {PARTNER_LOGOS.map((logo) => (
+          <SwiperSlide key={logo}>
+            <NextImage src={`/partners/${logo}`} alt={normalizePartnerLogoName(logo)} width={60} height={60} />
           </SwiperSlide>
         ))}
       </Swiper>
@@ -86,10 +87,4 @@ const PartnersWrapper = styled(Container)`
       opacity: 1;
     }
   }
-`;
-
-const PartnerLink = styled.a`
-  display: block;
-  text-decoration: none;
-  color: inherit;
 `;
