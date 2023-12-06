f40f21081c20c5bb5dfa248df8fe3b24e0f3f4ad
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2023-12-06 22:27:05 +0530

    Fixing the swipe (#11)
    * Added the Projects Section (#3)
    
    * feat: packages change
    
    * "Updated Community Partners"
    
    * all partners not qualified yet
    
    * Update Partners.tsx
    
    * updated the community partrner links
    
    ---------
    
    Co-authored-by: ABHIRUP <abhirupsaha02@gmail.com>
    
    * resolving errors (#10)
    
    * Added the Projects Section (#3) (#6)
    
    * feat: packages change
    
    * "Updated Community Partners"
    
    * all partners not qualified yet
    
    * Update Partners.tsx
    
    * updated the community partrner links
    
    ---------
    
    Co-authored-by: Abhi-codes <abhirupsaha02@gmail.com>
    
    * modifications (#7)
    
    * "Updated Community Partners"
    
    * all partners not qualified yet
    
    * Update Partners.tsx
    
    * "Adding the Projects Section"
    
    * updated the community partrner links
    
    * Added the Projects Section (#3)
    
    * feat: packages change
    
    * "Updated Community Partners"
    
    * all partners not qualified yet
    
    * Update Partners.tsx
    
    * updated the community partrner links
    
    ---------
    
    Co-authored-by: ABHIRUP <abhirupsaha02@gmail.com>
    
    * Projects section modified with design updates.
    
    * Updating the Project's Metadata
    
    * Updating the List for Projects
    
    * added community partners
    
    * modifications
    
    ---------
    
    Co-authored-by: Abhi-codes <abhirupsaha02@gmail.com>
    Co-authored-by: debarshee2004 <debarsheechakraborty.11d@gmail.com>
    
    * Test (#8)
    
    * "Updated Community Partners"
    
    * all partners not qualified yet
    
    * Update Partners.tsx
    
    * "Adding the Projects Section"
    
    * updated the community partrner links
    
    * Added the Projects Section (#3)
    
    * feat: packages change
    
    * "Updated Community Partners"
    
    * all partners not qualified yet
    
    * Update Partners.tsx
    
    * updated the community partrner links
    
    ---------
    
    Co-authored-by: ABHIRUP <abhirupsaha02@gmail.com>
    
    * Projects section modified with design updates.
    
    * Updating the Project's Metadata
    
    * Updating the List for Projects
    
    * added community partners
    
    * modifications
    
    ---------
    
    Co-authored-by: Abhi-codes <abhirupsaha02@gmail.com>
    Co-authored-by: debarshee2004 <debarsheechakraborty.11d@gmail.com>
    
    * fixing errors
    
    ---------
    
    Co-authored-by: Abhi-codes <abhirupsaha02@gmail.com>
    Co-authored-by: debarshee2004 <debarsheechakraborty.11d@gmail.com>
    
    * error fixes
    
    * fixing the swipe
    
    ---------
    
    Co-authored-by: Abhi-codes <abhirupsaha02@gmail.com>
    Co-authored-by: debarshee2004 <debarsheechakraborty.11d@gmail.com>

--- views/HomePage/Partners.tsx
+++ views/HomePage/Partners.tsx
@@ -4,33 +4,33 @@ import SwiperCore, { Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Container from 'components/Container';
 import { media } from 'utils/media';
-
 import 'swiper/css';
 import 'swiper/css/autoplay';
 import 'swiper/css/navigation';
 
 const PARTNER_LOGOS = [
   { name: 'SC CSE.png', link: 'https://sccsebio.vercel.app/?utm_source=sc+cse' },
-  { name: 'Samarth.png',link: 'https://www.samarthtmsl.live/' },
+  { name: 'Samarth.png', link: 'https://www.samarthtmsl.live/' },
   { name: 'GDSC NIET.png', link: 'https://gdsc.community.dev/noida-institute-of-engineering-and-technology-greater-noida/' },
   { name: 'Techsurfers.png', link: 'https://www.linkedin.com/company/tech-surfers/' },
   { name: 'GFG_NIET.png', link: 'https://linktr.ee/gfgniet' },
   { name: 'GDSC_Asansol.png', link: 'https://www.linkedin.com/company/gdsc-aec' },
 ];
 
+SwiperCore.use([Autoplay]);
 
 export default function Partners() {
-  SwiperCore.use([Autoplay])
   return (
     <PartnersWrapper>
       <Title>Community partners</Title>
       <Swiper
-        slidesPerView={5}
+        slidesPerView={6}
         centeredSlides={true}
-        spaceBetween={30}
+        spaceBetween={20} 
         loop={true}
         autoplay={{ delay: 0, disableOnInteraction: false, pauseOnMouseEnter: false }}
         speed={3000}
+        freeMode={true} 
         breakpoints={{
           320: { slidesPerView: 2 },
           768: { slidesPerView: 4 },
@@ -39,8 +39,19 @@ export default function Partners() {
         modules={[Autoplay]}
         className="swiper-wrapper"
       >
-        {PARTNER_LOGOS.map((partner) => (
-          <SwiperSlide key={partner.name}>
+        {PARTNER_LOGOS.map((partner, index) => (
+          <SwiperSlide key={index}>
+            {partner.link ? (
+              <PartnerLink href={partner.link} target="_blank" rel="noopener noreferrer">
+                <NextImage src={`/partners/${partner.name}`} alt={normalizePartnerLogoName(partner.name)} width={80} height={60} />
+              </PartnerLink>
+            ) : (
+              <NextImage src={`/partners/${partner.name}`} alt={normalizePartnerLogoName(partner.name)} width={80} height={60} />
+            )}
+          </SwiperSlide>
+        ))}
+        {PARTNER_LOGOS.map((partner, index) => (
+          <SwiperSlide key={index + PARTNER_LOGOS.length}>
             {partner.link ? (
               <PartnerLink href={partner.link} target="_blank" rel="noopener noreferrer">
                 <NextImage src={`/partners/${partner.name}`} alt={normalizePartnerLogoName(partner.name)} width={80} height={60} />
