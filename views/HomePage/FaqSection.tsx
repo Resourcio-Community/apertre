dd2d66a68dea6c71b132b4f0e5ca06f919afffaf
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2023-12-09 19:17:20 +0530

    "added faq section"

--- views/HomePage/FaqSection.tsx
+++ views/HomePage/FaqSection.tsx
@@ -4,42 +4,42 @@ import SectionTitle from 'components/SectionTitle';
 
 export default function FaqSection() {
   return (
-    <Wrapper>
-      <SectionTitle>Frequently asked question</SectionTitle>
-      <Accordion title="Eiusmod eu laboris qui minim incididunt et id in elit veniam Lorem nulla ullamco.">
-        Voluptate ad aliquip in adipisicing incididunt officia. Aliqua consectetur id commodo fugiat sunt dolor minim. Aliqua ut deserunt
-        sit irure tempor esse labore elit commodo pariatur in nisi minim culpa. Exercitation eu in mollit tempor cillum excepteur
-        adipisicing cillum do et dolor est deserunt dolor. Deserunt reprehenderit nulla commodo quis et consectetur deserunt ea eiusmod
-        reprehenderit aliqua. Cupidatat commodo do ad ullamco ad enim sint sint irure amet culpa. Consequat sit Lorem sint culpa.
+    <Wrapper id="faq">
+      <SectionTitle>Most <span style={{ color: '#fbce1f' }}> Frequently</span> Asked Questions</SectionTitle>
+      <Accordion title="What is Apertre, and who organizes it?">
+        Apertre is a 15-day long open-source event organized by Resourcio Community and GDSC AOT. It provides a platform for participants to
+        enhance their development skills through collaboration on real-world projects.
       </Accordion>
-      <Accordion title="Minim eiusmod aliquip in ea mollit fugiat ullamco veniam enim sint exercitation.">
-        Lorem laboris ullamco labore consectetur duis amet excepteur esse labore sunt pariatur do aliqua amet. Enim exercitation labore
-        nostrud consectetur veniam eiusmod pariatur do commodo dolore pariatur amet veniam. Irure do et ut dolor quis reprehenderit anim
-        aute sint deserunt tempor sunt nostrud. Deserunt et aute non dolor.
+      <Accordion title="How can I participate in Apertre?">
+        To participate in Apertre, you can visit the official event website and follow the registration process. Keep an eye out for
+        announcements from Resourcio Community and GDSC AOT for registration details.
       </Accordion>
-      <Accordion title="Commodo officia dolore anim occaecat.">
-        Enim ea cillum pariatur incididunt incididunt sunt ea exercitation nisi aliquip. Cillum deserunt ut esse Lorem non qui labore labore
-        eiusmod magna incididunt ut quis. Nulla excepteur adipisicing ipsum do non occaecat.
+      <Accordion title="What development areas are covered in Apertre?">
+        Apertre covers a wide range of development areas, allowing participants to choose from various domains such as web development,
+        mobile app development, machine learning, and more.
       </Accordion>
-      <Accordion title="Irure Lorem proident esse enim.">
-        Ipsum laboris sit minim occaecat nisi qui. Tempor eiusmod fugiat occaecat labore. Exercitation do cillum cillum laborum velit
-        laborum ea magna duis laborum in anim. Enim laboris dolore laboris consectetur enim duis deserunt consequat excepteur ipsum
-        excepteur. Non dolor magna et est cillum sint eiusmod sit eiusmod veniam id. Aliqua non non labore amet laborum fugiat ad nostrud ex
-        officia exercitation.
+      <Accordion title="Are there any prerequisites to join Apertre?">
+        Apertre is open to participants of all skill levels. Whether you are a beginner or an experienced developer, you are welcome to
+        join. However, certain projects may have specific skill requirements, so participants should review project details before choosing.
       </Accordion>
-      <Accordion title="Ea quis consequat sunt sint aute pariatur aliquip minim voluptate.">
-        Sint eiusmod aute nulla ut esse sunt fugiat nulla exercitation duis pariatur. Magna anim incididunt magna id quis irure laborum sint
-        voluptate tempor esse elit dolore. Elit exercitation pariatur adipisicing aute. Nisi deserunt enim consequat anim adipisicing
-        laborum sint occaecat elit. Consequat officia cupidatat magna incididunt nisi in sunt duis reprehenderit ullamco in amet cillum.
-        Dolor nisi proident ipsum aute velit aliquip magna fugiat reprehenderit. Enim ex aliqua et velit exercitation reprehenderit veniam
-        do eu exercitation ad duis.
+      <Accordion title="How does collaboration work during Apertre?">
+        Participants will have the opportunity to collaborate with others on real-world projects. The event fosters a collaborative
+        environment where participants can join teams, share ideas, and work together to achieve project goals.
+      </Accordion>
+      <Accordion title="Will there be mentorship available during Apertre?">
+        Yes, Apertre provides participants with mentorship and guidance from experienced developers. Mentors will be available to assist
+        participants, answer questions, and provide valuable insights throughout the event.
+      </Accordion>
+      <Accordion title="Can I showcase the projects I work on during Apertre?">
+        Absolutely! Apertre encourages participants to showcase their projects. There may be opportunities for project presentations or
+        demos, allowing participants to share their work with the community and potentially gain recognition for their contributions.
       </Accordion>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-  margin-top: 15rem;
+  margin: 10vw;
   & > *:not(:first-child) {
     margin-top: 3rem;
   }
