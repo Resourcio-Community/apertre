2f933af319f8edb4ef5f01e7e975dddf0b8cb166
ABHIRUP <abhirupsaha02@gmail.com>
2024-02-21 10:49:48 +0530

    update

---
+++ leaderboard/fetch.js
@@ -0,0 +1,20 @@
+const { Queue } = require('bullmq')
+const { writeFile } = require('./writeFile')
+require('dotenv').config()
+
+const queue = new Queue('apertre', {
+    connection: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        username: 'default',
+        password: process.env.REDIS_PASSWORD
+    }
+})
+
+async function fetchLeaderboard() {
+    const job = await queue.getJob('leaderboard')
+    if (!job) process.exit()
+    writeFile(job.data)
+}
+
+fetchLeaderboard()
